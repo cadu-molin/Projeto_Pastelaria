b85a7334b27b89cb6f9531bd1f414b1b4d20051f
cadu-molin <82542175+cadu-molin@users.noreply.github.com>
2021-10-12 08:41:03 -0300

    Primeiro Commit

---
+++ src/com/pastelaria/cliente/Principal.java
@@ -0,0 +1,7 @@
+package br.com.pastelaria.cliente;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/com/pastelaria/shared/BasicoCRUD.java
@@ -0,0 +1,5 @@
+package br.com.pastelaria.shared;
+
+public interface BasicoCRUD {
+
+}
