91cddde6eba1d80232fca9ac29f4d1493515c374
siluane <zancanarosilu@gmail.com>
2021-10-12 22:47:26 -0300

    commitumpessoa

--- src/com/pastelaria/shared/Pessoa.java
+++ src/com/pastelaria/shared/Pessoa.java
@@ -1,5 +1,15 @@
-package br.com.pastelaria.shared;
+package com.pastelaria.shared;
 
 public abstract class Pessoa {
-
+	protected String Nome;
+	protected String Usuário;
+	protected String Senha;
+	protected int Celular;
+	protected String Email;
+	protected String Cidade;
+	protected String Bairro;
+	protected String Rua;
+	protected int Numerocasa;
+	protected String Complemento;
+	protected String Observação;
 }
